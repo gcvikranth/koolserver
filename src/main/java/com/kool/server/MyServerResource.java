deff954020cc840bedaa6f2277ee84c2b3227f1a
gujjav <gcvikranth@yahoo.com>
2019-07-06 23:29:39 +0530

    Server changes

--- src/main/java/com/kool/server/MyServerResource.java
+++ src/main/java/com/kool/server/MyServerResource.java
@@ -8,10 +8,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.logging.Logger;
+//@CrossOrigin(origins = "*,http://localhost:3000", allowedHeaders = "*")
+//@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 
-@CrossOrigin(origins = { "http://localhost:3050", "http://localhost:4200" })
+@CrossOrigin(origins = {"*"})
 @RestController
 public class MyServerResource {
+	private Logger log(){
+		return  Logger.getLogger("MyServerResource class");
+
+	}
 
 	@Autowired
 	private HardcodedService hardCodedManagementService;
@@ -23,6 +30,9 @@ public class MyServerResource {
 
 	@GetMapping("/instructors/{username}/students")
 	public List<Student> getAllStudents(@PathVariable String username) {
+		log().info(" ## #Get all students");
+		System.out.println(" **** ## #get all student ##### ***8");
+
 		return hardCodedManagementService.findAllStudents();
 	}
 	@GetMapping("/instructors/{username}/courses/{id}")
@@ -45,15 +55,38 @@ public class MyServerResource {
 		return ResponseEntity.notFound().build();
 	}
 
+	@DeleteMapping("/instructors/{username}/students/{id}")
+	public ResponseEntity<Void> deleteStudent(@PathVariable String username, @PathVariable long id) {
+
+		Student student = hardCodedManagementService.deleteStudentById(id);
+
+		if (student != null) {
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 	@PutMapping("/instructors/{username}/courses/{id}")
 	public ResponseEntity<Course> updateCourse(@PathVariable String username, @PathVariable long id,
 			@RequestBody Course course) {
+
+		log().info(" ## #Update course");
 
 		Course courseUpdated = hardCodedManagementService.save(course);
 
 		return new ResponseEntity<Course>(course, HttpStatus.OK);
 	}
 
+	@PutMapping("/instructors/{username}/students/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable String username, @PathVariable long id,
+											   @RequestBody Student student) {
+		log().info(" ## #Updastudent");
+
+		Student studentUpdated = hardCodedManagementService.saveStudent(student);
+
+		return new ResponseEntity<Student>(student, HttpStatus.OK);
+	}
 	@PostMapping("/instructors/{username}/courses")
 	public ResponseEntity<Void> createCourse(@PathVariable String username, @RequestBody Course course) {
 
@@ -67,5 +100,20 @@ public class MyServerResource {
 
 		return ResponseEntity.created(uri).build();
 	}
+	@PostMapping("/instructors/{username}/students")
+	public ResponseEntity<Void> createStudent(@PathVariable String username, @RequestBody Student student) {
+		log().info("Adding new student"+student.getStudentName());
+
+		Student createdStudent = hardCodedManagementService.saveStudent(student);
+
+		// Location
+		// Get current resource url
+		/// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdStudent.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 
 }
